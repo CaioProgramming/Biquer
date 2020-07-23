b389a5f91309d8732fab468ae7e6f62380eeeef6
CaioProgramming <caioalvesferreiracontato@gmail.com>
2020-07-23 01:17:45 -0300

    Service card ui fixes

--- lib/components/service/ServiceCard.dart
+++ lib/components/service/ServiceCard.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:Biquer/constants.dart';
 import 'package:Biquer/model/service/Service.dart';
 import 'package:Biquer/model/service/ServiceData.dart';
@@ -30,7 +28,7 @@ class ServiceCard extends StatelessWidget {
         children: [
           Center(
             child:
-            Text('Você precisa voltar e informar os dados do seu serviço'),
+                Text('Você precisa voltar e informar os dados do seu serviço'),
           )
         ],
       ),
@@ -51,32 +49,43 @@ class ServiceCard extends StatelessWidget {
       child: Stack(
         children: [
           Positioned(
-            bottom: 10,
-            left: 10,
-            right: 10,
-            child: ClipRRect(
-              borderRadius: kDefaultBorder,
-              child: Container(
-                padding: EdgeInsets.all(8),
-                color: Utils.barcolor(context).withOpacity(0.40),
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 25, sigmaY: 25),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ServiceCardAutoText(
-                        servicename: service.name,
-                        textColor:
-                            TinyColor.fromString(service.style().textColor)
-                                .color,
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 10),
-                        child: Text(Utils.moneyText(service.value)),
-                      )
-                    ],
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  gradient: LinearGradient(
+                      begin: Alignment.bottomLeft,
+                      end: Alignment.topLeft,
+                      colors: [
+                        Colors.black.withOpacity(0.40),
+                        Colors.transparent
+                      ])),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ServiceCardAutoText(
+                    servicename: service.name,
+                    textColor:
+                    TinyColor
+                        .fromString(service
+                        .style()
+                        .textColor)
+                        .color,
                   ),
-                ),
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    decoration: BoxDecoration(
+                        color: Theme
+                            .of(context)
+                            .scaffoldBackgroundColor,
+                        borderRadius: kDefaultBorder),
+                    child: Text(Utils.moneyText(service.value)),
+                  )
+                ],
               ),
             ),
           )
